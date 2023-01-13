8217608f4c4512195b34c315b779d952e6e74bea
Marianna Zurek <zurekmarianna@gmail.com>
2023-01-13 19:54:16 +0100

    updated styling

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -5,6 +5,8 @@ const Option = ({ optionsToSelect, placeholder }) => {
   const [selectedOptionDraft, setSelectedOptionDraft] = useState({});
   const [selectedOptionSubbmited, setSelectedOptionSubbmited] = useState({});
   const [isOptionsOpen, setIsOptionsOpen] = useState(false);
+  const [internalSuggestions, setInternalSugestions] =
+    useState(optionsToSelect);
 
   useEffect(() => {
     window.addEventListener("click", () => setIsOptionsOpen(false));
@@ -13,6 +15,7 @@ const Option = ({ optionsToSelect, placeholder }) => {
   const onOptionSelect = (option) => {
     setSelectedOption(option);
     setSelectedOptionDraft(option);
+    setSelectedOptionSubbmited(option);
   };
 
   const onOptionDelete = () => {
@@ -42,42 +45,47 @@ const Option = ({ optionsToSelect, placeholder }) => {
   };
 
   return (
-    <>
-      <div className="option_container">
-        <div className="option__selected">
-          <input
-            className="option__selected-input"
-            value={selectedOptionDraft.name}
-            placeholder={placeholder}
-            onChange={onOptionDraftEdit}
-            onKeyUp={onOptionSubmit}
-            onClick={(event) => {
-              setIsOptionsOpen(true);
-              event.stopPropagation();
-            }}
-          />
+    <div className="option_container">
+      <div className="option__choosen-container">
+        <div className="option__choosen">Choosen option:</div>
+        <div className="option__choosen-element">
           {selectedOptionDraft.name && (
-            <span className="option__delete" onClick={onOptionDelete}>
-              &#10005;
-            </span>
+            <div style={{ display: "flex", gap: "5px" }}>
+              <span>{selectedOptionSubbmited.name}</span>
+              <span className="option__delete" onClick={onOptionDelete}>
+                &#10005;
+              </span>
+            </div>
           )}
         </div>
-        {isOptionsOpen && (
-          <ul className="option__list">
-            {optionsToSelect.map((option) => (
-              <li
-                className="option__list-element"
-                key={option.id}
-                onClick={() => onOptionSelect(option)}
-              >
-                {option.name}
-              </li>
-            ))}
-          </ul>
-        )}
       </div>
-      <div>{selectedOptionSubbmited.name}</div>
-    </>
+      <div className="option__selected">
+        <input
+          className="option__selected-input"
+          value={selectedOptionDraft.name}
+          placeholder={placeholder}
+          onChange={onOptionDraftEdit}
+          onKeyUp={onOptionSubmit}
+          onClick={(event) => {
+            setIsOptionsOpen(true);
+            event.stopPropagation();
+          }}
+        />
+      </div>
+      {isOptionsOpen && (
+        <ul className="option__list">
+          {optionsToSelect.map((option) => (
+            <li
+              className="option__list-element"
+              key={option.id}
+              onClick={() => onOptionSelect(option)}
+            >
+              {option.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
